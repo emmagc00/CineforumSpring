d071fe862cb6505c38c9e8e73c7f9dc36b3347b8
Emilio Ingenito <emilio.ingenito@icloud.com>
2021-06-14 11:44:14 +0200

    rimossi import

--- src/main/java/it/uniroma3/siw/cineforum/repository/PrenotazioneRepository.java
+++ src/main/java/it/uniroma3/siw/cineforum/repository/PrenotazioneRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.cineforum.model.Prenotazione;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 public interface PrenotazioneRepository extends CrudRepository<Prenotazione, Long>{
